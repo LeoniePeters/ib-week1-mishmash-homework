730af93636e53042ac5b6eecb5b4438665a5322e
LeoniePeters <leoniepeters@gmail.com>
2019-01-19 11:46:50 +0100

    create function addSomePromises

--- src/async-functions.js
+++ src/async-functions.js
@@ -6,12 +6,20 @@ function giveItBackLater(value,callback) {
 
 }
 
-function addSomePromises() {
+function addSomePromises(somePromise) {
+    somePromise(value)
+        .then(value => valuevalue)
+        .catch(value => valuevaluevalue)
 
 }
 
-function promiseToGiveItBackLater() {
-
+function promiseToGiveItBackLater(value) {
+    return new Promise((resolve) => {
+        function loadComplete() {
+            resolve(value)
+        }
+        setTimeout(loadComplete, 200)    
+    })
 }
 
 module.exports = {giveItBackLater, addSomePromises, promiseToGiveItBackLater}
